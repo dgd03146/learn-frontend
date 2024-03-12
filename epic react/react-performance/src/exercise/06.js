a0b355d73877a0915dcdb9ad960249db35c151e6
dgd03146 <ibory1220@naver.com>
2024-03-12 23:50:27 +0000

    Slice of App State with HOC

--- epic react/react-performance/src/exercise/06.js
+++ epic react/react-performance/src/exercise/06.js
@@ -105,12 +105,34 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
-function Cell({row, column}) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
-  return <CellImpl cell={cell} row={row} column={column} />
+function withStateSlice(Comp, slice) {
+  const MemoComp = React.memo(Comp)
+  function Wrapper(props, ref) {
+    const state = useAppState()
+    return <MemoComp ref={ref} state={slice(state, props)} {...props} />
+  }
+  Wrapper.displayName = `withStateSlice(${Comp.displayName || Comp.name})`
+  return React.memo(React.forwardRef(Wrapper))
 }
-Cell = React.memo(Cell)
+
+function Cell({state: cell, row, column}) {
+  const dispatch = useAppDispatch()
+  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+  return (
+    <button
+      className="cell"
+      onClick={handleClick}
+      style={{
+        color: cell > 50 ? 'white' : 'black',
+        backgroundColor: `rgba(0, 0, 0, ${cell / 100})`,
+      }}
+    >
+      {Math.floor(cell)}
+    </button>
+  )
+}
+
+Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
 
 function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
