b42bbd1b2d80cca96e5e1556827524ab199d8441
dgd03146 <ibory1220@naver.com>
2024-03-25 02:57:38 +0000

    receving only valid component props based on the generic prop

--- polymorphic react component/src/App.tsx
+++ polymorphic react component/src/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div>
       <h1>Hello Polymorphic Component</h1>
       <Text as="h1">H1 Component</Text>
-      <Text as="h2">H2 Component</Text>
+      <Text as="h2" href="hihi">
+        H2 Component
+      </Text>
       <Text as="p">Paragraph Component</Text>
-      <Text>Span Component</Text>
+      <Text as="a" href="hihi">
+        Span Component
+      </Text>
     </div>
   );
 }
